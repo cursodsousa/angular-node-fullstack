37393ebf49a3b2226bee2558e6f44d96ad4cb07e
Dougllas Sousa <cursodsousa@gmail.com>
2020-08-23 17:11:30 -0300

    criação do projeto

---
+++ meusprojetos/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PrimeiroComponent } from './primeiro-componente/primeiro.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ meusprojetos/src/app/primeiro-componente/primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+
+@Component({
+    selector : 'primeiro',
+    templateUrl : './primeiro.component.html'
+})
+export class PrimeiroComponent {
+    
+}
